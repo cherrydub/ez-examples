8aca999cac858744758f2dcec5985d34d0b916be
cherrydub <cherrydub@gmail.com>
2024-05-12 00:42:13 +0100

    taking out emjis for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,27 +11,27 @@ export default function Home() {
         <a href="https://nextjs.org/" target="_blank">
           Next.js
         </a>
-        , this is more than just a blog—it's a digital journal chronicling my
-        coding odyssey.
+        , this is more than just a blog—it&apos;s a digital journal chronicling
+        my coding odyssey.
       </p>
       <br />
       <p>
         Here, I invite you to join me on this exciting journey through the world
-        of coding. Whether it's diving into intriguing projects, jotting down
-        essential notes, or creating step-by-step code tutorials, this space is
-        my canvas for sharing and learning.
+        of coding. Whether it&apos;s diving into intriguing projects, jotting
+        down essential notes, or creating step-by-step code tutorials, this
+        space is my canvas for sharing and learning.
       </p>
       <br />
       <p>
-        Why did I create this hub, you ask? Well, it's not just for you; it's
-        for me too! Consider this a personal coding vault where I document my
-        projects and insights. A quick refresher for future me, and a helpful
-        resource for you.
+        Why did I create this hub, you ask? Well, it&apos;s not just for you;
+        it&apos;s for me too! Consider this a personal coding vault where I
+        document my projects and insights. A quick refresher for future me, and
+        a helpful resource for you.
       </p>
       <br />
       <p>
-        So buckle up, explore the code-filled expanse, and let's embark on this
-        coding adventure together!
+        So buckle up, explore the code-filled expanse, and let&apos;s embark on
+        this coding adventure together! 🚀💻
       </p>
       <br />
       <h5 className="text-xl font-bold">Some of my sites:</h5>
